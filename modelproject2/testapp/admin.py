3c296ff979afb73804f3994fc31f062d258e4fb7
vikashkumarnirala <vikashnirala78@gmail.com>
2024-06-03 12:51:40 +0530

    delete modelproject2

--- modelproject2/testapp/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from testapp.models import employee
-# Register your models here.
-class employeeAdmin(admin.ModelAdmin):
-    list_display=['id','eno','ename','esal','eaddr']
-admin.site.register(employee,employeeAdmin)    
